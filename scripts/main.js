f88a13be46ca61f6f2eab9f46d03c2a428ff92cd
WantechFan <alifiandratsaqifp@gmail.com>
2026-06-06 13:25:31 +0700

    Jawaskrip

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const evilliquids = Vars.mods.locateMod("evil-liquids").meta;
+
+evilliquids.displayName = Core.bundle.get("mod.evil-liquids.name");
+evilliquids.author = Core.bundle.get("mod.evil-liquids.author");
+evilliquids.description = Core.bundle.get("mod.evil-liquids.description");
+evilliquids.subtitle = Core.bundle.get("mod.evil-liquids.subtitle");
